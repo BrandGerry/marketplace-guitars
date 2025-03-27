266677ec5248ea2aa9f2f02d3bdef65c1a853fa2
BrandGerry <91706030+BrandGerry@users.noreply.github.com>
2025-03-26 18:25:43 -0600

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,46 @@
+import { useEffect, useState } from "react";
+import Guitar from "./components/Guitar";
+import Header from "./components/Header";
+import { db } from "./helpers/Db";
+
+function App() {
+  const [data, setData] = useState(db);
+  const [cart, setCart] = useState([]);
+
+  function addToCart(iteam) {
+    const iteamExist = cart.findIndex((guitar) => guitar.id === iteam.id);
+    if (iteamExist >= 0) {
+      const updatedCart = [...cart];
+      updatedCart[iteamExist].quantity++;
+      setCart(updatedCart);
+    } else {
+      iteam.quantity = 1;
+      setCart([...cart, iteam]);
+    }
+  }
+
+  return (
+    <>
+      <Header cart={cart} />
+      <main className="container-xl mt-5">
+        <h2 className="text-center">Nuestra Colección</h2>
+
+        <div className="row mt-5">
+          {data.map((guitar) => (
+            <Guitar guitar={guitar} key={guitar.id} addToCart={addToCart} />
+          ))}
+        </div>
+      </main>
+
+      <footer className="bg-dark mt-5 py-5">
+        <div className="container-xl">
+          <p className="text-white text-center fs-4 mt-4 m-md-0">
+            GuitarLA - Todos los derechos Reservados
+          </p>
+        </div>
+      </footer>
+    </>
+  );
+}
+
+export default App;
